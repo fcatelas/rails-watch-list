899b8aecb7e43b15d7e7983298b74699d5df0662
francois catelas <catelas.francois@gmail.com>
2024-11-29 10:42:43 +0100

    Add dotenv-rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.2/dist/js/tom-select.complete.js"
-pin "star-rating.js", to: "https://ga.jspm.io/npm:star-rating.js@4.3.0/dist/star-rating.esm.js"
